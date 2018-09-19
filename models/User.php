8c05a2b6d424558c67e3aaa8f4e51ed20aefb149
anknigaWork <ankniga@gmail.com>
2018-09-19 18:37:26 +0300

    php storm

--- models/User.php
+++ models/User.php
@@ -89,6 +89,7 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     public function validateAuthKey($authKey)
     {
         return $this->authKey === $authKey;
+
     }
 
     /**
